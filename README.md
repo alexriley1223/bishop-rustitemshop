918c3e2e8eafe9d1872035442459e7b1adb32768
Alex Riley <alexriley1223@gmail.com>
2023-10-12 18:18:21 -0400

    chore: add README; add banner; formatting

---
+++ README.md
@@ -0,0 +1,27 @@
+<div align="center">
+	<p>
+		<img src="https://raw.githubusercontent.com/alexriley1223/bishop-rustitemshop/rework/static/bishopbanner.png" alt="Bishop Rust Item Shop Banner" />
+	</p>
+	<br />
+	<p>
+		<img src="https://img.shields.io/github/license/alexriley1223/bishop-rustitemshop" />
+		<img src="https://img.shields.io/github/v/tag/alexriley1223/bishop-rustitemshop"/>
+		<img src="https://img.shields.io/badge/Bishop-version-7046ff" />
+	</p>
+</div>
+
+## Bishop Rust Item Shop
+
+A module for the [Bishop Discord Bot](https://github.com/alexriley1223/bishop) that automatically posts in a specified channel whenever a new [Rust](https://rust.facepunch.com/) item shop releases on Thursday. Module starts polling for new item shops at 2pm EST.
+
+### Requirements
+- Bishop Discord Bot
+
+### Installation
+1. Clone or download zip of repository and place in modules directory of the Bishop bot.
+2. Copy `config_template.json` to `config.json`. Fill out all fields.
+3. Run `npm install` on root Bishop bot to add required packages for this module.
+4. Restart bot.
+
+### Issues
+This repository is actively maintained by me. If you run into any issues, feel free to <a href="https://github.com/alexriley1223/bishop-rustitemshop/issues/new">create a new issue here</a>.
--- jobs/pollNewRustItemShop.js
+++ jobs/pollNewRustItemShop.js
@@ -10,77 +10,80 @@ let pullItemDataInterval;
 
 module.exports = function(client) {
 
-    const job = {};
+	const job = {};
 
 	job.enabled = jobs[getParentDirectoryString(__filename, __dirname, 'jobs')];
 
 	job.executeJob = function() {
 		new CronJob(
-            '0 14 * * 4',
-            async function() {
-                pullItemDataInterval = setInterval(function() {
-                    getScmmShop(client);
-                }, 900000); // Every 15 minutes keep checking
-                getScmmShop(client);
-            },
-            null,
-            true,
-            'America/Indianapolis');
-	}
+			'0 14 * * 4',
+			async function() {
+				pullItemDataInterval = setInterval(function() {
+					getScmmShop(client);
+				}, 900000); // Every 15 minutes keep checking
+				getScmmShop(client);
+			},
+			null,
+			true,
+			'America/Indianapolis');
+	};
 
-    return job;
+	return job;
 };
 
 function getScmmShop(client) {
-    log.info('Rust Item Store', '✅ Pulling item store data.')
-    axios.get('https://rust.scmm.app/api/store/current')
-    .then(response => {
-        const newShopId = response.data.id;
-        const newShopImage = response.data.itemsThumbnailUrl;
-        let oldShopExisted = true;
+	log.info('Rust Item Store', '✅ Pulling item store data.');
+	axios.get('https://rust.scmm.app/api/store/current')
+		.then(response => {
+			const newShopId = response.data.id;
+			const newShopImage = response.data.itemsThumbnailUrl;
+			let oldShopExisted = true;
 
-        /* Check if old shop file exists */
-        if (!fs.existsSync(__dirname + '/../old_shop.txt')) {
-            fs.writeFileSync(__dirname + '/../old_shop.txt', `${newShopId}`, (err) => {
-                if(err) {
-                    throw Error('❌ Failed to create old shop file.');
-                } else {
-                    log.info('Rust Item Store', '✅ Successfully created old shop file.')
-                    oldShopExisted = false;
-                }
-            });
-        }
+			/* Check if old shop file exists */
+			if (!fs.existsSync(__dirname + '/../old_shop.txt')) {
+				fs.writeFileSync(__dirname + '/../old_shop.txt', `${newShopId}`, (err) => {
+					if (err) {
+						throw Error('❌ Failed to create old shop file.');
+					}
+					else {
+						log.info('Rust Item Store', '✅ Successfully created old shop file.');
+						oldShopExisted = false;
+					}
+				});
+			}
 
-        const now = new Date();
-        if(oldShopExisted) {
-            const oldShopId = fs.readFileSync(__dirname + '/../old_shop.txt', 'utf8');
+			const now = new Date();
+			if (oldShopExisted) {
+				const oldShopId = fs.readFileSync(__dirname + '/../old_shop.txt', 'utf8');
 
-            if(oldShopId != newShopId) {
-                clearInterval(pullItemDataInterval);
-                client.channels.cache.get(rustChannelId).send({
-                    files: [
-                        {attachment: `${newShopImage}`, name: `item_shop_${now.toDateString()}.png`},
-                    ],
-                    content: `Rust Item Store for ${now.toDateString()}`
-                });
-                fs.writeFileSync(__dirname + '/../old_shop.txt', `${newShopId}`, (err) => {
-                    if(err) {
-                        throw Error('❌ Failed to update old shop file.');
-                    } else {
-                        log.info('Rust Item Store', '✅ Successfully updated old shop file.')
-                    }
-                });
-            }
-        } else {
-            client.channels.cache.get(rustChannelId).send({
-                files: [
-                    {attachment: `${newShopImage}`, name: `item_shop_${now.toDateString()}.png`},
-                ],
-                content: `Rust Item Store for ${now.toDateString()}`
-            });
-        }
-    })
-    .catch(error => {
-        log.error(`Rust Item Shop', '❌ Failed to fetch item shop API.\n${error}`);
-    });
+				if (oldShopId != newShopId) {
+					clearInterval(pullItemDataInterval);
+					client.channels.cache.get(rustChannelId).send({
+						files: [
+							{ attachment: `${newShopImage}`, name: `item_shop_${now.toDateString()}.png` },
+						],
+						content: `Rust Item Store for ${now.toDateString()}`,
+					});
+					fs.writeFileSync(__dirname + '/../old_shop.txt', `${newShopId}`, (err) => {
+						if (err) {
+							throw Error('❌ Failed to update old shop file.');
+						}
+						else {
+							log.info('Rust Item Store', '✅ Successfully updated old shop file.');
+						}
+					});
+				}
+			}
+			else {
+				client.channels.cache.get(rustChannelId).send({
+					files: [
+						{ attachment: `${newShopImage}`, name: `item_shop_${now.toDateString()}.png` },
+					],
+					content: `Rust Item Store for ${now.toDateString()}`,
+				});
+			}
+		})
+		.catch(error => {
+			log.error(`Rust Item Shop', '❌ Failed to fetch item shop API.\n${error}`);
+		});
 }
